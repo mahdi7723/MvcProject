6e605667ee160dfa8cf6da860c2dbedc572756fe
mahdi <mahdiparvizi77@yahoo.com>
2022-06-11 18:52:25 +0430

    change link in login.blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <link rel="stylesheet" href="{{mix('\css\login') }}">
+    <link rel="stylesheet" href="{ mix('css/login.css') }}">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
